23691a370ea53ac97559487982f0119488a6b77f
roguediv <47837581+roguediv@users.noreply.github.com>
2024-11-09 17:37:50 -0600

    image caching

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
   images: {
     domains: ['webapp.jacobmiranda.com'], // Add your domain here
   },
+  async headers() {
+    return [
+      {
+        source: '/images/uploads/:path*', // Matches all images in the uploads directory
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store', // Ensures the browser does not cache images
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
