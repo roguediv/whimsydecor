26530df69a83b23807d6b6dd945efa8d110e63cf
roguediv <47837581+roguediv@users.noreply.github.com>
2024-11-17 09:26:58 -0600

    mobile setActive fix

--- components/parts/cms/main/CmsNav.tsx
+++ components/parts/cms/main/CmsNav.tsx
@@ -13,68 +13,12 @@ const CMSNav: React.FC<props> = ({className = '', login=false}) => {
   const [isLoaded, setIsLoaded] = useState(false);
   
   useEffect(() => {
-    const navElement = document.querySelector('nav');
-    const handleAnchorClick = (event: Event) => {
-      const clickedAnchor = event.target as HTMLAnchorElement;
-      const mainElement = document.querySelector('main');
-      
-      navElement?.classList.add("phone-fade-out");
-      setTimeout(() => {
-        navElement?.classList.remove("activePhone", "phone-fade-out");
-        mainElement?.classList.remove("nav-active")
-      }, 250)
-    };
-
-    const anchorElements = navElement?.querySelectorAll("a");
-
-    if (anchorElements) {
-      anchorElements.forEach((anchor) => {
-        if (anchor.classList.contains('ignore')) return;
-        anchor.addEventListener("click", handleAnchorClick);
-      });
-    }
-    
-    const buttonElements = navElement?.querySelectorAll("button");
-
-    if (buttonElements) {
-      buttonElements.forEach((button) => {
-        if (button.classList.contains('ignore')) return;
-        button.addEventListener("click", handleAnchorClick);
-      });
-    }
-
     const timer = setTimeout(() => {
       setIsLoaded(true);
     }, 750);
-    
-    return () => {
-      const anchorElements = navElement?.querySelectorAll("a");
-      clearTimeout(timer);
-
-      if (anchorElements) {
-        anchorElements.forEach((anchor) => {
-          if (anchor.classList.contains('ignore')) return;
-          anchor.removeEventListener("click", handleAnchorClick);
-        });
-      }
-    };
+    return () => {clearTimeout(timer);};
   }, []);
 
-  function handleNavToggle() {
-    const navElement = document.querySelector('nav');
-    const mainElement = document.querySelector('main');
-    const burger = document.querySelector('nav .hamburger');
-    if (navElement?.classList.contains("activePhone")) {
-      navElement?.classList.add("phone-fade-out");
-      setTimeout(() => {
-        navElement?.classList.remove("activePhone", "phone-fade-out");
-        mainElement?.classList.remove("nav-active")
-      }, 250)
-    } else {
-      navElement?.classList.add("activePhone");
-      mainElement?.classList.add("nav-active")
-    }
-  }
   if (login) {
     return (
       <nav className={`${className} cms-login ${isLoaded ? 'animate' : ''}`}>
